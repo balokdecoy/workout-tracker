bd9f7f157936255a8a3dda2e47673d50ffcf5344
johnniesimpson <johnniesimpson@outlook.com>
2021-03-13 11:21:37 -0500

    Create route files. Set up server.

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+const api = require("./public/api");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(express.static("seeders"));
+
+mongoose.connect(process.env.MONGODB_URI || 
+    "mongodb://localhost/workout", 
+    { useNewUrlParser: true }
+);
+
+require("./routes/html-routes")(app);
+require("./routes/workout-routes")(app);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}...`);
+});
